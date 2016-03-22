7c784e1d8b48795fe6e1a57a53868c0904cc0063
meeksfred <maxfred0025@gmail.com>
2016-03-22 14:23:36 -0700

    todos finished, repos loading

--- class-12-REST-and-APIs/pair-assignment/mike-max-brandon/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/mike-max-brandon/scripts/repo.js
@@ -5,7 +5,16 @@
 
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax({
+      url: 'https://api.github.com/users/mevans72/repos?per_page=5&sort=updated',
+      type: 'GET',
+      headers: { 'Authorization': 'token ' + githubToken },
+      success: function(data, message, xhr) {
+        repos.all = data;
 
+        callback();
+      }
+    });
   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
